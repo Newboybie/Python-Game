7e1dfc7c046e16543fad3b73260e53007616bda7
My name <me@example.com>
2024-11-11 14:38:59 +0700

    Make sure the enemy alaway facing the player

--- code/enemy.py
+++ code/enemy.py
@@ -11,3 +11,13 @@ class Enemy(Entity):
         for sprite in collision_sprites.sprites():           # Đảm bảo kẻ địch xuất hiện đúng vị trí
             if sprite.rect.collidepoint(self.rect.midbottom):
                 self.rect.bottom = sprite.rect.top
+
+    def get_status(self):                                    # Kẻ địch sẽ luôn quay mặt về hướng người chơi
+        if self.player.rect.centerx < self.rect.centerx:
+            self.status = 'left'
+        else:
+            self.status = 'right'
+
+    def update(self,dt):
+        self.get_status()
+        self.animate(dt)
