f9a7be7d27b17eab606ad4c3d48a9a525ff3f7d6
My name <me@example.com>
2024-11-12 13:45:27 +0700

    Add health bar for the player

--- code/overlay.py
+++ code/overlay.py
@@ -4,6 +4,10 @@ class Overlay:
     def __init__(self, player):
         self.player = player
         self.display_surface = pygame.display.get_surface()
+        self.health_surf = pygame.image.load('D:/Python-Game/graphics/health.png').convert_alpha()
 
     def display(self):
-        self.display_surface.blit(self.player.image, (0, 0))
+        for h in range(self.player.health):                  # In từng thanh máu tại vị trí x, y
+            x = 10 + h * (self.health_surf.get_width() + 4)
+            y = 10
+            self.display_surface.blit(self.health_surf, (x, y))
